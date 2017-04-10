3c45e15fe8d8bd52efa8115aa866db54d368b1b3
Steve Strong <sstrong@apprenticesystems.com>
2017-04-09 20:12:38 -0400

    now with routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
+import { NetworkviewComponent } from './networkview/networkview.component';
+import { ListviewComponent } from './listview/listview.component';
+import { TreeviewComponent } from './treeview/treeview.component';
+import { WelcomeComponent } from "app/welcome/welcome.component";
+
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', children: [] },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'tree', component: TreeviewComponent },
+  { path: 'list', component: ListviewComponent },
+  { path: 'network', component: NetworkviewComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { TreeviewComponent } from './treeview/treeview.component';
 import { ListviewComponent } from './listview/listview.component';
 import { NetworkviewComponent } from './networkview/networkview.component';
+import { TreeitemComponent } from './treeview/treeitem.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TreeviewComponent,
     ListviewComponent,
-    NetworkviewComponent
+    NetworkviewComponent,
+    TreeitemComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
