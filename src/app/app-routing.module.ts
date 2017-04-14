a6c7d6903718351043102c2a6312e65f759b78c5
Steve Strong <sstrong@apprenticesystems.com>
2017-04-14 08:40:48 -0400

    force diagram complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,11 @@ import { ListviewComponent } from './listview/listview.component';
 import { TreeviewComponent } from './treeview/treeview.component';
 import { WelcomeComponent } from "../app/welcome/welcome.component";
 import { WebglviewComponent } from './webglview/webglview.component';
-import { TestSceen1Component } from './webglview/test-sceen1.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'welcome' },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'webgl', component: WebglviewComponent },
-  { path: 'test1', component: TestSceen1Component },
   { path: 'tree', component: TreeviewComponent },
   { path: 'list', component: ListviewComponent },
   { path: 'network', component: NetworkviewComponent }
