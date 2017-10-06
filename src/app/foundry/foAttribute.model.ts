ad3cda2bd4b5614a6981282673be6308fe6e6de9
585448 <strong_stephen@bah.com>
2017-10-06 19:52:56 -0400

    refresh of foundry

--- src/app/foundry/foAttribute.model.ts
+++ src/app/foundry/foAttribute.model.ts
@@ -1,13 +1,35 @@
-import { foObject }  from './foObject.model'
 
+import { foKnowledge } from './foKnowledge.model'
 
- 
-export class foAttribute extends foObject {
+export class foAttribute extends foKnowledge {
 
-  constructor() {
-    super();
+  constructor(spec: any = undefined) {
+    super(spec);
     this.myType = 'foAttribute';
   }
-  
+
+}
+
+export class foViewAttribute extends foAttribute {
+
+  private _title:string;
+  private _mySource: foAttribute = undefined;
+
+  constructor(source: foAttribute, spec: any = undefined) {
+    super(spec);
+    this._mySource = source;
+    this.myType = 'foViewAttribute';
+  }
+
+//https://toddmotto.com/typescript-setters-getter
+
+  get title() {
+    return this._title ? this._title : this.myName;
+  }
+
+  set title(name) {
+    this._title = name;
+  }
+
 }
 
