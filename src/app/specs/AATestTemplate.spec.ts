dbb41f0553bca4616477c37e40e1a971ef763f36
Steve <strong_stephen@bah.com>
2017-11-21 21:03:46 -0500

    better not great

--- src/app/specs/AATestTemplate.spec.ts
+++ src/app/specs/AATestTemplate.spec.ts
@@ -2,25 +2,25 @@
 import { Tools } from '../foundry/foTools'
 import { foComponent } from '../foundry/foComponent.model'
 
-describe("Foundry: Test Template", function () {
-    var obj;
+// describe("Foundry: Test Template", function () {
+//     var obj;
 
-    beforeEach(function () {
-        obj = fo.makeComponent({});
-        return obj;
-    });
+//     beforeEach(function () {
+//         obj = fo.makeComponent({});
+//         return obj;
+//     });
      
-    it("should be a component", function () {
-        expect(fo.utils.isaComponent(obj)).toBe(true);
+//     it("should be a component", function () {
+//         expect(fo.utils.isaComponent(obj)).toBe(true);
 
-        expect(obj.myName).toBeUndefined();
-        expect(obj.myParent).toBeUndefined();
+//         expect(obj.myName).toBeUndefined();
+//         expect(obj.myParent).toBeUndefined();
 
-        expect(obj.Properties.isEmpty()).toBe(true);
-        expect(obj.Properties.count).toEqual(0);
+//         expect(obj.Properties.isEmpty()).toBe(true);
+//         expect(obj.Properties.count).toEqual(0);
 
-        expect(obj.Subcomponents.isEmpty()).toBe(true);
-        expect(obj.Subcomponents.count).toEqual(0);
-    });
+//         expect(obj.Subcomponents.isEmpty()).toBe(true);
+//         expect(obj.Subcomponents.count).toEqual(0);
+//     });
 
-});
+// });
--- src/app/specs/GetSpecAndHydrationTest.js
+++ src/app/specs/GetSpecAndHydrationTest.js
@@ -1,8 +1,4 @@
-﻿/// <reference path="../Scripts/jasmine-1.3.1/jasmine.js" />
-
-/// <reference path="../Foundry/Foundry.trace.js" />
-/// <reference path="../Foundry/Foundry.core.js" />
-/// <reference path="../Foundry/Foundry.rules.factory.js" />
+﻿
 
 describe("Foundry: GetSpec", function () {
     var obj;
