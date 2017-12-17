b49798df99e1cdc2ccc635fcedcd74a9a50ab81a
Steve Strong <sstrong@apprenticesystems.com>
2017-12-16 20:35:17 -0500

    renderhit

--- src/app/foundry/foInterface.ts
+++ src/app/foundry/foInterface.ts
@@ -67,7 +67,6 @@ export interface iBox {
 
 export interface iShape extends iNode {
     render(ctx: CanvasRenderingContext2D, deep:boolean): void;
-    renderHitTest(ctx: CanvasRenderingContext2D): void;
     findObjectUnderPoint(hit: iPoint, deep:boolean, ctx: CanvasRenderingContext2D): iShape;
     findObjectUnderShape(hit: iShape, deep:boolean, ctx: CanvasRenderingContext2D): iShape;
     draw(ctx: CanvasRenderingContext2D): void;
