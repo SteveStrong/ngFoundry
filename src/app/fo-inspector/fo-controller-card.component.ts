703d3c67c524552bbd6c25fe195a99f36c79d9ac
Steve Strong <sstrong@apprenticesystems.com>
2018-03-20 08:08:27 -0400

    new controller card

---
+++ src/app/fo-inspector/fo-controller-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fo-controller-card',
+  templateUrl: './fo-controller-card.component.html',
+  styleUrls: ['./fo-controller-card.component.css']
+})
+export class foControllerCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
