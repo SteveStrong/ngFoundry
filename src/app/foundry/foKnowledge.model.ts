9751d2e4625d86ec462e3bc11e3321b9779eb5d0
Steve Strong <sstrong@apprenticesystems.com>
2018-04-23 12:51:12 -0400

    more linting

--- src/app/foundry/foKnowledge.model.ts
+++ src/app/foundry/foKnowledge.model.ts
@@ -1,108 +1,114 @@
-import { Tools, foNames } from './foTools'
+import { Tools, foNames } from './foTools';
 
-import { WhereClause, Action } from "./foInterface";
+import { WhereClause, Action } from './foInterface';
 
-import { foObject } from './foObject.model'
-
+import { foObject } from './foObject.model';
 
 export class foKnowledge extends foObject {
-    private static _counter: number = 0;
+  private static _counter: number = 0;
 
-    constructor(properties?: any, parent?: foKnowledge) {
-        super(properties, parent);
+  constructor(properties?: any, parent?: foKnowledge) {
+    super(properties, parent);
+  }
+
+  public initialize(
+    x: number = Number.NaN,
+    y: number = Number.NaN,
+    ang: number = Number.NaN
+  ) {
+    return this;
+  }
+
+  usingRuntimeType(type: string, action: Action<foKnowledge>) {
+    action(this);
+    return this;
+  }
+
+  makeComponent(parent?: any, properties?: any, onComplete?: Action<any>): any {
+    return undefined;
+  }
+
+  newInstance(properties?: any, subcomponents?: any, parent?: any): any {
+    return undefined;
+  }
+
+  extract(target: any) {
+    let result = {};
+    return result;
+  }
+
+  get commands(): Array<string> {
+    return [];
+  }
+
+  specReadWriteKeys(): string[] {
+    return [];
+  }
+
+  defaultName(name?: string) {
+    if (name) {
+      this.myName = name;
+    } else if (Tools.matches(this.myName, foNames.UNKNOWN)) {
+      foKnowledge._counter += 1;
+      let count = ('0000' + foKnowledge._counter).slice(-4);
+      this.myName = `${this.myType}_${count}`;
     }
+    return this;
+  }
 
-    public initialize(x: number = Number.NaN, y: number = Number.NaN, ang: number = Number.NaN) {
-        return this;
+  get displayName() {
+    if (this._displayName) return this._displayName;
+    if (Tools.matches(this.myName, foNames.UNKNOWN)) {
+      return this.defaultName().myName;
     }
+    return this.myName;
+  }
+  set displayName(value: string) {
+    this._displayName = value;
+  }
 
-    usingRuntimeType(type: string, action: Action<foKnowledge>) {
-        action(this);
-        return this;
+  select(
+    where: WhereClause<foKnowledge>,
+    list?: foCollection<foKnowledge>,
+    deep: boolean = true
+  ): foCollection<foKnowledge> {
+    let result = list ? list : new foCollection<foKnowledge>();
+    let self = <foKnowledge>this;
+    if (where(self)) {
+      result.addMember(self);
     }
+    return result;
+  }
 
-    makeComponent(parent?: any, properties?: any, onComplete?:Action<any>): any {
-        return undefined;
+  extractCopySpec(keys?: string[]) {
+    let spec = this.asJson;
+    keys &&
+      keys.forEach(key => {
+        spec[key] = this[key];
+      });
+    return spec;
+  }
+
+  createCopy(keys?: string[]) {
+    let data = this.extractCopySpec(keys);
+    let { myType } = data;
+
+    let type = RuntimeType.find(myType);
+    if (type) {
+      let copy = RuntimeType.construct(type, data);
+      return copy;
     }
+  }
 
-    newInstance(properties?: any, subcomponents?: any, parent?: any): any {
-        return undefined;
-    }
+  public reHydrate(json: any) {
+    this.override(json);
+    return this;
+  }
 
-    extract(target: any) {
-        let result = {};
-        return result;
-    }
-
-    get commands(): Array<string> {
-        return [];
-    }
-
-    specReadWriteKeys():string[] { 
-        return [];
-    }
-
-    defaultName(name?: string) {
-        if (name) {
-            this.myName = name;
-        }
-        else if (Tools.matches(this.myName, foNames.UNKNOWN)) {
-            foKnowledge._counter += 1;
-            let count = ("0000" + foKnowledge._counter).slice(-4);
-            this.myName = `${this.myType}_${count}`;
-        }
-        return this;
-    }
-
-    get displayName() {
-        if (this._displayName) return this._displayName;
-        if (Tools.matches(this.myName, foNames.UNKNOWN)) {
-            return this.defaultName().myName;
-        }
-        return this.myName;
-    }
-    set displayName(value: string) {
-        this._displayName = value;
-    }
-
-    select(where: WhereClause<foKnowledge>, list?: foCollection<foKnowledge>, deep: boolean = true): foCollection<foKnowledge> {
-        let result = list ? list : new foCollection<foKnowledge>();
-        let self = <foKnowledge>this;
-        if (where(self)) {
-            result.addMember(self)
-        }
-        return result;
-    }
-
-    extractCopySpec(keys?:string[]){
-        let spec = this.asJson;
-        keys && keys.forEach(key => {
-            spec[key] = this[key];
-        })
-        return spec;
-    }
-
-    createCopy(keys?:string[]) {
-        let data = this.extractCopySpec(keys);
-        let { myType } = data;
-
-        let type = RuntimeType.find(myType);
-        if (type) {
-            let copy = RuntimeType.construct(type,data);
-            return copy;
-        }
-    }
-
-    public reHydrate(json: any) {
-        this.override(json);
-        return this;
-    }
-
-    public deHydrate(context?: any, deep: boolean = true) {
-        let data = this.extractCopySpec();
-        return data;
-    }
-
+  public deHydrate(context?: any, deep: boolean = true) {
+    let data = this.extractCopySpec();
+    return data;
+  }
 }
 
 import { RuntimeType } from './foRuntimeType';
